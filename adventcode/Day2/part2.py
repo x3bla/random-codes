fb9b22b2c1385476be2839b0ed0c91495b3f343f
x3bla <75881405+x3bla@users.noreply.github.com>
2021-11-03 11:20:29 +0800

    .

--- adventcode/Day2/part2.py
+++ adventcode/Day2/part2.py
@@ -10,8 +10,8 @@ with open("inputDay2.txt", 'r') as f:
 
 for number in checkNumbers:
     number = number.strip()  # removing empty line  10-18 l: gllpmlgtrmnllhllrlll
-    key_and_letter, pw = number.split(": ")  # splitString = ["10-18 l", "gllpmlgtrmnllhllrlll"]
-    ranges, letter = key_and_letter.split(" ")  # y = ["10-18", "l"]
+    range_and_key, pw = number.split(": ")  # splitString = ["10-18 l", "gllpmlgtrmnllhllrlll"]
+    ranges, letter = range_and_key.split(" ")  # y = ["10-18", "l"]
     range1, range2 = ranges.split("-")  # [10, 18]
 
     if pw[int(range1)-1] == letter or pw[int(range2)-1] == letter:  # if 10 <= amount of letter <= 18
